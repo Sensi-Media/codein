94cd3b3c4308e168e53f7a9bb6047dddce3270a8
Marijn Ophorst <marijn@monomelodies.nl>
2019-03-10 09:55:50 +0100

    doccomments checker. todo: check if @param and @return (and possibly @throws?) are correct

---
+++ src/Doccomments.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sensi\Codein;
+
+use ReflectionClass;
+use Generator;
+
+class Doccomments extends Check
+{
+    public function check(string $file) : Generator
+    {
+        if (!($class = $this->extractClass($file))) {
+            return;
+        }
+        $reflection = new ReflectionClass($class);
+        if (!$reflection->getDocComment()) {
+            yield "<red>Class <darkRed>$class <red>is missing doccomment in <darkRed>$file";
+        }
+        foreach ($reflection->getMethods() as $method) {
+            if ($method->getDeclaringClass()->name != $class) {
+                continue;
+            }
+            if ($method->getFileName() != $reflection->getFileName()) {
+                continue;
+            }
+            if (!$method->getDocComment()) {
+                yield "<red>Method <darkRed>{$method->name} <red>is missing doccomment in <darkRed>$file";
+            }
+        }
+    }
+}
+
