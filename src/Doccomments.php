ded5db05a53fa4a9c07e6253bce9ae68f342d7a3
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-25 16:03:55 +0100

    add doccomments

--- src/Doccomments.php
+++ src/Doccomments.php
@@ -5,8 +5,18 @@ namespace Sensi\Codein;
 use ReflectionClass;
 use Generator;
 
+/**
+ * Check if the class and all its methods have a doccomment.
+ * TODO: also check this for properties...
+ */
 class Doccomments extends Check
 {
+    /**
+     * Run the check.
+     *
+     * @param string $file
+     * @return Generator
+     */
     public function check(string $file) : Generator
     {
         if (!($class = $this->extractClass($file))) {
