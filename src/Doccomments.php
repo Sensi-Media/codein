bf71e5ab0a0e50425dac7f7959d78ada93cf621d
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-25 13:17:55 +0100

    we don't have the filename anymore, and we don't really need it either

--- src/Doccomments.php
+++ src/Doccomments.php
@@ -14,17 +14,20 @@ class Doccomments extends Check
         }
         $reflection = new ReflectionClass($class);
         if (!$reflection->getDocComment()) {
-            yield "<red>Class <darkRed>$class <red>is missing doccomment in <darkRed>$file";
+            yield "<red>Class <darkRed>$class <red>is missing doccomment";
         }
         foreach ($reflection->getMethods() as $method) {
             if ($method->getDeclaringClass()->name != $class) {
+                var_dump($method->getDeclaringClass()->name, $class);
+                var_dump('class');
                 continue;
             }
             if ($method->getFileName() != $reflection->getFileName()) {
+                var_dump('method');
                 continue;
             }
             if (!$method->getDocComment()) {
-                yield "<red>Method <darkRed>{$method->name} <red>is missing doccomment in <darkRed>$file";
+                yield "<red>Method <darkRed>$class::{$method->name} <red>is missing doccomment";
             }
         }
     }
