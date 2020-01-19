888d509bf8fcf104ffa07ab71c48488716cd873b
Marijn Ophorst <marijn@monomelodies.nl>
2020-01-19 15:28:34 +0100

    new check for on-the-fly properties

---
+++ src/Properties.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Sensi\Codein;
+
+use ReflectionClass;
+use Generator;
+use Ansi;
+
+/**
+ * Check if all properties on the class are correctly defined and nothing is
+ * "set on the fly".
+ */
+class Properties extends Check
+{
+    /**
+     * Run the check.
+     *
+     * @param string $file
+     * @return Generator
+     */
+    public function check(string $file) : Generator
+    {
+        if (!($class = $this->extractClass($file))) {
+            return;
+        }
+        $reflection = new ReflectionClass($class);
+        if ($reflection->isAbstract()) {
+            return;
+        }
+        $args = [];
+        if ($constructor = $reflection->getConstructor()) {
+            foreach ($constructor->getParameters() as $parameter) {
+                fwrite(STDOUT, Ansi::tagsToColors("<darkGreen>$class<green> constructor argument <darkGreen>\${$parameter->name}<green> value: <reset>"));
+                $argument = trim(fgets(STDIN));
+                $args[] = strlen($argument) ? $argument : null;
+            }
+        }
+        $instance = $reflection->newInstance(...$args);
+        $properties = [];
+        foreach ($reflection->getProperties() as $property) {
+            $properties[$property->name] = true;
+        }
+        foreach ($instance as $property => $value) {
+            if (!isset($properties[$property])) {
+                yield "<red>Property <darkRed>$class->{$property->name} <red>is added on the fly in <darkRed>{$this->file}";
+            }
+        }
+    }
+}
+
